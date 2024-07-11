de7cd33640840b70158987777d2a8190358dc255
Hannarong <114913567+saran1185@users.noreply.github.com>
2024-07-11 16:24:30 +0700

    8.2 8.3
    Create the file name type.ts and declaration the type of event

---
+++ Lab2/src/type.ts
@@ -0,0 +1,11 @@
+export interface Event {
+  id: number
+  categort: string
+  title: string
+  description:string
+  location:string
+  date:string
+  time:string
+  petsAllowed:boolean
+  organizer:string
+}
