a22899f0447ccea63e621fa8e2f325adbbae7369
Hannarong <114913567+saran1185@users.noreply.github.com>
2024-07-19 11:01:11 +0700

    Lab 3 1.1
    create EventDetaolView.vue

---
+++ Lab2/src/views/EventDetailView.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+import Event from '@/type/Event'
+const event = ref<Event>(null)
+onMounted(() => {
+    // fectch event (by id) and set local event data 
+})
+</script>
+<template>
+    <div v-if = "event">
+        <h1>{{ event.title }}</h1>
+        <p>{{ event.time }} on {{  event.data }} @ {{ event.location }}</p>
+        <p>{{ event.description }}</p>
+    </div>
+</template>
